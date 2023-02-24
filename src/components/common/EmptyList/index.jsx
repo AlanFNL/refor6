91342510b9adc8973524ab31ff28c48c21641e44
AlanFNL <alanfnl09@gmail.com>
2023-02-24 12:47:57 -0300

    deleted dummy blog items

--- src/components/common/EmptyList/index.jsx
+++ src/components/common/EmptyList/index.jsx
@@ -2,10 +2,6 @@ import React from "react";
 import "./styles.css";
 import empty from "../../../assets/13525-empty.gif";
 
-const EmptyList = () => (
-  <div className="emptyList-wrap">
-    <img src={empty} alt="empty" />
-  </div>
-);
+const EmptyList = () => <div className="emptyList-wrap"></div>;
 
 export default EmptyList;
